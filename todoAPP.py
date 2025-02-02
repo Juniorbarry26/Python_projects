606f3447064c90552cdff216c9b89805752e01a1
Alsainey Barry <alsainey7@gmail.com>
2025-02-02 17:40:46 +0000

    Minor changes to the todo app

--- todoAPP.py
+++ todoAPP.py
@@ -1,4 +1,66 @@
-user_weight = int(input("Enter your weight in pounds? "))
-user_weight_inKG = user_weight * 0.453592
+import os
 
-print("You weight is ", user_weight_inKG , "KG")
+todo_list = []
+
+def show_menu():
+    print("\n To-Do List Application")
+    print("1. View Task")
+    print("2. Add Task")
+    print("3. Complete Task")
+    print("4. Delete Task")
+    print("5. Exist")
+
+def view_task():
+    if not todo_list:
+        print("No task available")
+    else:
+        for index, task in enumerate(todo_list, start=1):
+            print(f"{index}. {task}")
+def add_task():
+    task = input("Enter the task: ")
+    todo_list.append(task)
+    print("Task added successfully")
+def complete_task():
+    view_task()
+    try:
+        task_num = int(input("Enter the task to mark as completed: "))
+        if 1 <= task_num <= len(todo_list):
+            todo_list.pop(task_num - 1)
+            print("Task marked as completed.")
+        else:
+            print("Invalid task number.")
+    except ValueError:
+        print("Please enter a valid number")
+
+def delete_task():
+    view_task()
+    try:
+        task_num = int(input("Enter the task number to delete"))
+        if 1 <= task_num <= len(todo_list):
+            del todo_list[task_num - 1]
+            print("Task deleted successfully")
+        else:
+            print("Invalid task number")
+    except ValueError:
+        print("Please enter a valid number")
+
+
+def main():
+    while True:
+        show_menu()
+        choice = input("Choose an input: ")
+        if choice == "1":
+            view_task()
+        elif choice == "2":
+            add_task()
+        elif choice == "3":
+            complete_task()
+        elif choice == "4":
+            delete_task()
+        elif choice == "5":
+            print("Goodbye!")
+        else:
+            print("Invalid choice, please try again.")
+
+if __name__ == "__main__":
+    main()
